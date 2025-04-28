85e4326d626168ad382fd975fb2ccf7ff8b4aa40
Piotr89waYT <pradko32@gmail.com>
2025-04-28 13:56:28 +0100

    Create main.py
    Hi

---
+++ main.py
@@ -0,0 +1,8 @@
+# "While loops go back on themselves. Thats all that loops are, they repeat themselves" - Maky, probably
+
+# Python Password Generator. It generates a Password for you bassed off a few user made inputs and then exports them into a text file.
+
+
+
+print("Hi")
+
